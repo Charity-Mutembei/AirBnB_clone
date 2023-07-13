aa7e324a4bc910a65a6a85d068b2dbf20fb9cb73
Charity-Mutembei <charity.softwaredev@gmail.com>
2023-07-13 04:14:37 +0300

    --ammend

--- models/retrieve_object.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""
-This is a file of the class RetrieveObject which will be responsible
-for the functionality of retriving the information created from files 
-or databases, and so on
-"""
-class RetrieveObject:
-    """The class holding the methods for retriving information"""
-    
