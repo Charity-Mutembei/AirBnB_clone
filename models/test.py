1bf06988784b609962ffb303ae8080eaf866d2cf
Charity-Mutembei <charity.softwaredev@gmail.com>
2023-07-17 04:08:08 +0300

    --ammend

--- models/test.py
+++ models/test.py
@@ -1,3 +1,18 @@
 from models import my_string
 
 print(my_string)
+
+    # def do_all(self, arg):
+    #     """Prints all string representation of all instances"""
+    #     args = arg.split()
+    #     objs = storage.all()
+    #     if not args:
+    #         print([str(obj) for obj in objs.values()])
+    #     else:
+    #         class_name = args[0]
+    #         if class_name in classes:
+    #             for key, obj in objs.items():
+    #                 if key.startswith(class_name):
+    #                     print(str(obj))
+    #         else:
+    #             print("** class doesn't exist **")
